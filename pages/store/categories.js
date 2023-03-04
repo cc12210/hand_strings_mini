e5c1a0de671b661013e4e90f7f4b5a33d9240fab
caocheng <2832913976@qq.com>
2023-03-04 19:30:21 +0800

    feat: 增加图片样式

---
+++ pages/store/categories.js
@@ -0,0 +1,268 @@
+export const categories = [{
+        "label": "相思红",
+        "title": "花心愁欲断，春色岂知心",
+        // colro: "",
+        "children": [{
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_red_1.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_red_2.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_red_3.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_red_4.jpg"
+            }
+        ]
+    },
+    {
+        "label": "雾松绿",
+        "title": "记得绿罗裙，处处怜芳草",
+        "children": [{
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_green_1.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_green_2.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_green_3.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_green_4.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_green_5.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_green_6.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_green_7.jpg"
+            }
+        ]
+    },
+    {
+        "label": "逍遥青",
+        "title": "春风知别苦，不谴柳条青",
+        "children": [{
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_1.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_2.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_3.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_4.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_5.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_6.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_7.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_ching_8.jpg"
+            }
+        ]
+    },
+    {
+        "label": "风雅紫",
+        "title": "暗暗淡淡紫，融融冶冶黄",
+        "children": [{
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_purple_1.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_purple_2.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_purple_3.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_purple_4.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_purple_5.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_purple_6.jpg"
+            }
+        ]
+    },
+    {
+        "label": "幻夜黑",
+        "title": "斫取青光写楚辞，腻香春粉黑离离",
+        "children": [{
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_black_1.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_black_2.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_black_3.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_black_4.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_black_5.jpg"
+            }
+        ]
+    },
+    {
+        "label": "瑰蜜粉",
+        "title": "盈盈粉色凌时，寒玉体、先透薄妆",
+        "children": [{
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_1.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_2.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_3.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_4.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_5.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_6.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_7.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_8.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_9.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_10.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_pink_11.jpg"
+            }
+        ]
+    },
+    {
+        "label": "五彩缤纷",
+        "children": [{
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_1.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_2.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_3.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_4.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_5.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_6.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_7.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_8.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_9.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_10.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_11.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_12.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_13.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_14.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_15.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_16.jpg"
+            },
+            {
+                "label": "",
+                "image": "http://image.unknown-host.com/hand_strings/hand_strings_colorful_17.jpg"
+            }
+        ]
+    }
+]
--- pages/store/store.js
+++ pages/store/store.js
@@ -1,47 +1,11 @@
-const image = 'https://tdesign.gtimg.com/miniprogram/images/example2.png';
-const items = new Array(12).fill({ label: '标题文字', image }, 0, 12);
+const cateList = require('./categories')
 
 Page({
   offsetTopList: [],
   data: {
     sideBarIndex: 1,
     scrollTop: 0,
-    categories: [
-      {
-        label: '选项选项选项选项',
-        title: '标题',
-        badgeProps: {},
-        items,
-      },
-      {
-        label: '选项',
-        title: '标题',
-        badgeProps: {
-          dot: true,
-        },
-        items: items.slice(0, 9),
-      },
-      {
-        label: '选项',
-        title: '标题',
-        badgeProps: {},
-        items: items.slice(0, 9),
-      },
-      {
-        label: '选项',
-        title: '标题',
-        badgeProps: {
-          count: 6,
-        },
-        items: items.slice(0, 6),
-      },
-      {
-        label: '选项',
-        title: '标题',
-        badgeProps: {},
-        items: items.slice(0, 3),
-      },
-    ],
+    categories: cateList.categories,
   },
   onLoad() {
     const query = wx.createSelectorQuery().in(this);
@@ -60,6 +24,7 @@ Page({
   },
   onScroll(e) {
     const { scrollTop } = e.detail;
+    // console.log(e.detail)
     const threshold = 50; // 下一个标题与顶部的距离
 
     if (scrollTop < threshold) {
